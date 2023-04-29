c50d1474f8e293d107c11fe26ee066a5567fe0db
IkerNoya <ikernoya4@gmail.com>
2023-04-28 23:50:28 -0300

    Created folders

---
+++ LD53/Assets/Script/Ship/ShipController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private float Acceleration = 5f;
+    [SerializeField] private float Speed = 10f;
+    [SerializeField] 
+    
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
