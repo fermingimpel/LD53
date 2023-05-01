0af9a13469694f8a605f1ab53428e5f223468f7e
fermin gimpel <fermingimpel@gmail.com>
2023-05-01 16:28:48 -0300

    Added menu assets

--- LD53/Assets/Script/Menu.cs
+++ LD53/Assets/Script/Menu.cs
@@ -6,29 +6,39 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject optionsCanvas;
-    [SerializeField] private GameObject creditsCanvas;
     [SerializeField] private GameObject tutorialCanvas;
+    [SerializeField] private GameObject creditsCanvas;
+    [SerializeField] private GameObject baseCanvas;
 
     [SerializeField] private string gameSceneName;
 
 
-    public void ToggleOptionsCanvas()
+    private void Start()
     {
-        optionsCanvas.SetActive(!optionsCanvas.activeSelf);
+        InputManager.Instance.ChangeActionMapping("UI");
     }
 
-    public void ToggleCreditsCanvas()
+    public void ToggleOptionsCanvas()
     {
-        creditsCanvas.SetActive(!creditsCanvas.activeSelf);
+        baseCanvas.SetActive(optionsCanvas.activeSelf);
+        optionsCanvas.SetActive(!optionsCanvas.activeSelf);
     }
 
     public void ToggleTutorialCanvas()
     {
+        baseCanvas.SetActive(tutorialCanvas.activeSelf);
         tutorialCanvas.SetActive(!tutorialCanvas.activeSelf);
+    }
+
+    public void ToggleCreditsCanvas()
+    {
+        baseCanvas.SetActive(creditsCanvas.activeSelf);
+        creditsCanvas.SetActive(!creditsCanvas.activeSelf);
     }
 
     public void LoadGame()
     {
+        InputManager.Instance.ChangeActionMapping("Player");
         SceneManager.LoadScene(gameSceneName);
     }
 
