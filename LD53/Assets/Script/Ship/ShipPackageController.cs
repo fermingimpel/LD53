88c6c5f9fee5349a50307c70161977f24c44cf35
IkerNoya <ikernoya4@gmail.com>
2023-05-01 20:08:55 -0300

    added audio manager in menu

--- LD53/Assets/Script/Ship/ShipPackageController.cs
+++ LD53/Assets/Script/Ship/ShipPackageController.cs
@@ -75,7 +75,6 @@ public class ShipPackageController : MonoBehaviour
         remainingPackagesLeft--;
         if (remainingPackagesLeft <= 0)
         {
-            Debug.Log("HOLA");
             remainingPackagesLeft = 0;
             packageHUD.ToggleOutOfPackages();
         }
