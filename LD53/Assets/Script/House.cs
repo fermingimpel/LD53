9b03277338049f906c164abead56b8797893d681
TopoArania <matufabbri@gmail.com>
2023-05-01 19:24:38 -0300

    Created currentHouses list

--- LD53/Assets/Script/House.cs
+++ LD53/Assets/Script/House.cs
@@ -40,4 +40,9 @@ public class House : MonoBehaviour
             shipPackageController.PackageDelivered();
         }
     }
+
+    public bool GetIsCurrent()
+    {
+        return isCurrent;
+    }
 }
