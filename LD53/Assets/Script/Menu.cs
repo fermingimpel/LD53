4d5faee3670a83e448887f22fd5d6a2ad0d6f0f5
IkerNoya <ikernoya4@gmail.com>
2023-05-01 21:35:27 -0300

    menu fix

--- LD53/Assets/Script/Menu.cs
+++ LD53/Assets/Script/Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
@@ -9,6 +10,10 @@ public class Menu : MonoBehaviour
     [SerializeField] private GameObject tutorialCanvas;
     [SerializeField] private GameObject creditsCanvas;
     [SerializeField] private GameObject baseCanvas;
+    [SerializeField] private GameObject menuFirstSelection;
+    [SerializeField] private GameObject CreditsSelection;
+    [SerializeField] private GameObject HowToPlay;
+
 
     [SerializeField] private string gameSceneName;
 
@@ -16,22 +21,33 @@ public class Menu : MonoBehaviour
     private void Start()
     {
         InputManager.Instance.ChangeActionMapping("UI");
+        EventSystem.current.SetSelectedGameObject(menuFirstSelection);
     }
 
     public void ToggleOptionsCanvas()
     {
+        if(baseCanvas.activeSelf)
+            EventSystem.current.SetSelectedGameObject(menuFirstSelection);
         baseCanvas.SetActive(optionsCanvas.activeSelf);
         optionsCanvas.SetActive(!optionsCanvas.activeSelf);
     }
 
     public void ToggleTutorialCanvas()
     {
+        if(baseCanvas.activeSelf)
+            EventSystem.current.SetSelectedGameObject(menuFirstSelection);
+        else
+            EventSystem.current.SetSelectedGameObject(HowToPlay);
         baseCanvas.SetActive(tutorialCanvas.activeSelf);
         tutorialCanvas.SetActive(!tutorialCanvas.activeSelf);
     }
 
     public void ToggleCreditsCanvas()
     {
+        if(baseCanvas.activeSelf)
+            EventSystem.current.SetSelectedGameObject(menuFirstSelection);
+        else
+            EventSystem.current.SetSelectedGameObject(CreditsSelection);
         baseCanvas.SetActive(creditsCanvas.activeSelf);
         creditsCanvas.SetActive(!creditsCanvas.activeSelf);
     }
